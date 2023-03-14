09b128d002bd6039bc454036ac09a6b1fee4b3e8
Vo Minh Khang <itcsiu21136@student.hcmiu.edu.vn>
2023-03-15 00:18:09 +0700

    15/03/2023

--- src/view/MenuView.java
+++ src/view/MenuView.java
@@ -1,27 +1,30 @@
 package view;
 
+import java.awt.*;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JLayeredPane;
+
+import controller.Menu.MenuController;
+import controller.Menu.MenuMouseComingSoon;
+import controller.Menu.MenuMouseExitController;
+
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+
 import java.awt.BorderLayout;
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.BoxLayout;
 import javax.swing.JTextField;
 import javax.swing.JMenuItem;
 import javax.swing.JEditorPane;
 import java.awt.Toolkit;
 import java.awt.Font;
-import javafx.application.Application;
 
-public class MenuView extends JFrame{
+public class MenuView extends JFrame {
 
     private JPanel contentPane;
     private JTextField textField_1;
@@ -46,9 +49,15 @@ public class MenuView extends JFrame{
      * Create the frame.
      */
     public MenuView() {
+
+        MenuController menuController = new MenuController(this);
+        MenuMouseExitController menuMouseExitController = new MenuMouseExitController(this);
+        MenuMouseComingSoon menuMouseComingSoon = new MenuMouseComingSoon(this);
+
+        setResizable(false);
         setFont(new Font("Times New Roman", Font.PLAIN, 14));
         setIconImage(Toolkit.getDefaultToolkit().getImage(
-                "GameProject\\src\\view\\image\\png-clipart-plants-vs-zombies-256-icon-plants-vs-zombies-thumbnail - Edited.png"));
+                "src\\view\\image\\dausht0-74037f3a-028d-4ed0-ac0a-c39ccc4e7a34.png"));
         setTitle("Plants Vs Zombie Clone");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(805, 605);
@@ -66,6 +75,7 @@ public class MenuView extends JFrame{
 
         JMenuItem mntmNewMenuItem = new JMenuItem("Exit");
         mnNewMenu.add(mntmNewMenuItem);
+        mntmNewMenuItem.addActionListener(menuController);
 
         JEditorPane editorPane = new JEditorPane();
         mnNewMenu.add(editorPane);
@@ -92,41 +102,100 @@ public class MenuView extends JFrame{
 
         JPanel startAdventure = new JPanel();
         startAdventure.setBounds(407, 36, 320, 106);
+        
         panel.add(startAdventure);
 
         JPanel comingSoonTab = new JPanel();
         comingSoonTab.setBounds(407, 152, 295, 243);
+        comingSoonTab.addMouseListener(menuMouseComingSoon);
         panel.add(comingSoonTab);
 
         JPanel optionTab = new JPanel();
         optionTab.setBounds(546, 405, 95, 97);
+
         panel.add(optionTab);
 
         JPanel helpTab = new JPanel();
         helpTab.setBounds(651, 405, 48, 97);
+
         panel.add(helpTab);
 
         JPanel exitTab = new JPanel();
         exitTab.setBounds(709, 405, 67, 97);
+        exitTab.setLayout(new BorderLayout());
+        exitTab.addMouseListener(menuMouseExitController);
         panel.add(exitTab);
-        setResizable(false);
+
+        // JButton jButton_Exit = new JButton("Exit");
+        // exitTab.add(jButton_Exit, BorderLayout.CENTER);
+        // jButton_Exit.setVisible(true);
+        // jButton_Exit.setContentAreaFilled(false);
+        // jButton_Exit.setOpaque(false);
+        // jButton_Exit.setPreferredSize(new Dimension(0, 0));
+        // jButton_Exit.setBackground(new Color(0, 0, 0, 0));
+        // jButton_Exit.addActionListener(menuController);
+
+        // JButton jButton_Help = new JButton("Help");
+        // helpTab.add(jButton_Help, BorderLayout.CENTER);
+        // jButton_Help.setVisible(true);
+        // jButton_Help.setContentAreaFilled(false);
+        // jButton_Help.setOpaque(false);
+        // jButton_Help.setPreferredSize(new Dimension(0, 0));
+        // jButton_Help.setBackground(new Color(0, 0, 0, 0));
+        // jButton_Help.addActionListener(menuController);
+
+        // JButton jButton_Option = new JButton("Option");
+        // optionTab.add(jButton_Option, BorderLayout.CENTER);
+        // jButton_Option.setVisible(true);
+        // jButton_Option.setContentAreaFilled(false);
+        // jButton_Option.setOpaque(false);
+        // jButton_Option.setPreferredSize(new Dimension(0, 0));
+        // jButton_Option.setBackground(new Color(0, 0, 0, 0));
+        // jButton_Option.addActionListener(menuController);
+
+        // JButton jButton_ComingSoon = new JButton("ComingSoon");
+        // comingSoonTab.add(jButton_ComingSoon, BorderLayout.CENTER);
+        // jButton_ComingSoon.setVisible(true);
+        // jButton_ComingSoon.setContentAreaFilled(false);
+        // jButton_ComingSoon.setOpaque(false);
+        // jButton_ComingSoon.setPreferredSize(new Dimension(0, 0));
+        // jButton_ComingSoon.setBackground(new Color(0, 0, 0, 0));
+        // jButton_ComingSoon.addActionListener(menuController);
+
+        // JButton jButton_StartAdventure = new JButton("StartAdventure");
+        // startAdventure.add(jButton_StartAdventure, BorderLayout.CENTER);
+        // jButton_StartAdventure.setVisible(true);
+        // jButton_StartAdventure.setContentAreaFilled(false);
+        // jButton_StartAdventure.setOpaque(false);
+        // jButton_StartAdventure.setPreferredSize(new Dimension(0, 0));
+        // jButton_StartAdventure.setBackground(new Color(0, 0, 0, 0));
+        // jButton_StartAdventure.addActionListener(menuController);
+
+        // exitTab.remove(btnNewButton);
+        // exitTab.revalidate();
+        // exitTab.repaint();
 
         setVisible(true);
 
     }
-    public void startAdventure(){
+
+    public void startAdventure() {
 
     }
-    public void comingSoon(){
+
+    public void comingSoon() {
 
     }
-    public void optionTab(){
+
+    public void optionTab() {
 
     }
-    public void helpTab(){
+
+    public void helpTab() {
 
     }
-    public void exitTab(){
-        
+
+    public void exitTab() {
+        System.exit(0);
     }
 }
