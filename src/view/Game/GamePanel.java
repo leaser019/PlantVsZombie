c02f5384df9d4062ea19b52d4a5bab2dd5d78cd9
leaser362 <khangpro59)@.com>
2023-04-10 15:20:48 +0700

    10/4/23

--- src/view/Game/GamePanel.java
+++ src/view/Game/GamePanel.java
@@ -1,5 +1,29 @@
 package view.Game;
 
-public class GamePanel extend JPanel {
-    
+import javax.swing.JPanel;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
+
+public class GamePanel extends JPanel {
+    private Random random;
+    public GamePanel (){
+        random = new Random();
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        for(int x=0;x<20;x++){
+            for(int y=0;y<20;y++){
+            g.setColor(getRanColor());
+            g.fillRect(x*40, y*30, 40, 30);
+            }
+        }
+    }
+    public Color getRanColor(){
+        int r = random.nextInt(256);
+        int g = random.nextInt(256);
+        int b = random.nextInt(256);
+        return new Color(r,g,b);
+    }
 }
